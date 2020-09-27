4abd294c27b576d14a9ffd4d4313653959d9610d
Dani54197 <Dani54197@outlook.es>
2020-09-27 19:33:02 -0300

    Updates

---
+++ precache-manifest.d59e9a9fc7ece0fee9a6c0e8be5d63da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a24e0f8043a63ef1aca8144d4648896",
+    "url": "/react-bootstrap-practica/index.html"
+  },
+  {
+    "revision": "083364bce38358bf58b7",
+    "url": "/react-bootstrap-practica/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b11c81324a209a6b6ed9",
+    "url": "/react-bootstrap-practica/static/js/2.e6b1785d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bootstrap-practica/static/js/2.e6b1785d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "083364bce38358bf58b7",
+    "url": "/react-bootstrap-practica/static/js/main.815e1352.chunk.js"
+  },
+  {
+    "revision": "f858400406157119bb4c",
+    "url": "/react-bootstrap-practica/static/js/runtime-main.be4562fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-bootstrap-practica/static/media/logo.5d5d9eef.svg"
+  }
+]);
